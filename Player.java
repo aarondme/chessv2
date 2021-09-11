5cdb287c1c4762fe80bd6d11eddfc74a56e6b63f
Aaron D'Mello <aaron.dmello@hotmail.com>
2021-09-11 18:29:57 -0400

    Refactoring old code - TODO pairing algorithm

--- Player.java
+++ Player.java
@@ -1,10 +1,62 @@
 import java.util.LinkedList;
 
 public class Player{
-    int id;
-    String name;
-    String organization;
-    int score;
-    LinkedList<MatchResult> matchResults;
-    Tiebreaks tiebreaks;
+    private String name;
+    private String organization;
+    private int score;
+    private int gamesAsWhite, gamesAsBlack;
+    private LinkedList<MatchResult> matchResults;
+    private Tiebreaks tiebreaks;
+
+    Player(String name, String organization){
+        this.name = name;
+        this.organization = organization;
+        this.score = 0;
+        this.matchResults = new LinkedList<MatchResult>();
+        this.tiebreaks = new Tiebreaks();
+        this.gamesAsBlack = 0;
+        this.gamesAsWhite = 0;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getOrganization() {
+        return organization;
+    }
+    public int getScore() {
+        return score;
+    }
+    public LinkedList<MatchResult> getMatchResults() {
+        return matchResults;
+    }
+    public Tiebreaks getTiebreaks() {
+        return tiebreaks;
+    }
+    public int getGamesAsBlack() {
+        return gamesAsBlack;
+    }
+    public int getGamesAsWhite() {
+        return gamesAsWhite;
+    }
+    public String getDisplayName() {
+        if (organization.trim().length() == 0)
+          return name;
+        return (name + " (" + organization + ")");
+      }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+    public void addMatchResults(MatchResult matchResult){
+        matchResults.add(matchResult);
+        score += matchResult.getScore();
+    }
+    public void updateTiebreaks(LinkedList<MatchResult> matchResults){
+        tiebreaks.updateTiebreaks(matchResults);
+    }
 }
